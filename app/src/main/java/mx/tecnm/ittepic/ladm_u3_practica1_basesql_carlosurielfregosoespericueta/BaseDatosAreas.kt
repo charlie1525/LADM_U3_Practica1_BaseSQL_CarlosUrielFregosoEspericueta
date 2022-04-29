bc266570af52ddc8d45466640776e58b5f6dc9f2
Carlos Uriel Fregoso Espericueta <caurfregosoes@ittepic.edu.mx>
2022-04-28 19:53:56 -0600

    Adding the data base and uplodaing dependencies on gradle:app

--- app/src/main/java/mx/tecnm/ittepic/ladm_u3_practica1_basesql_carlosurielfregosoespericueta/BaseDatosAreas.kt
+++ app/src/main/java/mx/tecnm/ittepic/ladm_u3_practica1_basesql_carlosurielfregosoespericueta/BaseDatosAreas.kt
@@ -1,4 +1,36 @@
 package mx.tecnm.ittepic.ladm_u3_practica1_basesql_carlosurielfregosoespericueta
 
-class BaseDatosAreas {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BaseDatosAreas(context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int)
+    : SQLiteOpenHelper(context, name, factory, version){
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            "create table Area(" +
+                    "IdArea int  primary key autoincrement," +
+                    "Descripcion varchar(200)," +
+                    "Division varchar(50)," +
+                    "CaantidadEmpleados int);"
+        ) // fin de la creacion de la primer tabla
+
+        db.execSQL(
+            "create table Subdepartamento(" +
+                    "IdSubdepto int primary key autoincrement," +
+                    "IdEdificio varchar(20)," +
+                    "Piso varchar(20)." +
+                    "IdArea int references Area(IdArea)"+
+                    ");"
+        )// fin de la segunda consulta
+
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+    }
+
 }
